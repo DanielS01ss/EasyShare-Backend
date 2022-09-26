abf586c56dfc5154291ef669c41a57a1c219ec74
Daniel Stanculescu <danystanculescu@gmail.com>
2022-09-26 11:12:23 +0300

    Add signUp route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import App from "./app";
+import Authentication from "./controllers/Authentication";
 import HomeController from "./controllers/HomeController";
 import bodyParser from "body-parser";
 
 const app = new App({
     port:5000,
     controllers:[
-        new HomeController()
+        new HomeController(),
+        new Authentication()
     ],
     middleWares:[
         bodyParser.json(),
